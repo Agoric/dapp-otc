a25b33708ef4810c1ee5e87e3bed67c0a1d18d55
Kate Sills <kate@agoric.com>
2021-06-08 14:24:42 -0700

    chore: use the new reallocate API rather than trade (#18)
    * chore: use the new reallocate API rather than trade
    
    * chore: make .want clearer
    
    Co-authored-by: Chris Hibbert <Chris-Hibbert@users.noreply.github.com>

--- contract/src/otcDesk.js
+++ contract/src/otcDesk.js
@@ -1,7 +1,6 @@
 // @ts-check
 import '@agoric/zoe/exported';
 import {
-  trade,
   saveAllIssuers,
   assertProposalShape,
   withdrawFromSeat,
@@ -25,14 +24,10 @@ const start = async zcf => {
       /** @type OfferHandler */
       const addInventory = seat => {
         assertProposalShape(seat, { want: {} });
-        trade(
-          zcf,
-          {
-            seat: marketMakerSeat,
-            gains: seat.getCurrentAllocation(),
-          },
-          { seat, gains: {} },
+        marketMakerSeat.incrementBy(
+          seat.decrementBy(seat.getCurrentAllocation()),
         );
+        zcf.reallocate(marketMakerSeat, seat);
         seat.exit();
         return 'Inventory added';
       };
@@ -41,14 +36,9 @@ const start = async zcf => {
     makeRemoveInventoryInvitation: () => {
       /** @type OfferHandler */
       const removeInventory = seat => {
-        trade(
-          zcf,
-          {
-            seat: marketMakerSeat,
-            gains: {},
-          },
-          { seat, gains: seat.getProposal().want },
-        );
+        const { want: seatWant } = seat.getProposal();
+        seat.incrementBy(marketMakerSeat.decrementBy(seatWant));
+        zcf.reallocate(seat, marketMakerSeat);
         seat.exit();
         return 'Inventory removed';
       };
